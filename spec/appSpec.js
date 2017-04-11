925af3ea64379c1b3f666d62c3dd67c94df6be56
Stuart Harris <stuartaharris@gmail.com>
2017-04-10 23:38:38 -0500

    initiate boilerplate and tests

---
+++ spec/appSpec.js
@@ -0,0 +1,19 @@
+describe("Game", function () {
+  var turn=[],
+  counter,
+  points;
+  it('should contain the following variabes: ', function() {
+    expect(turn.toBeDefined());
+    expect(counter.toBeDefined());
+    expect(points.toBeDefined());
+  });
+  it('should have a turn array', function() {
+
+  });
+  it('should have a counter equal to the length of the array', function() {
+
+  });
+  it('should count points for each round completed', function() {
+
+  });
+});
