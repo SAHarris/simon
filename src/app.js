a3f4fe178fcc001dc3a1b9985ac0cd3cb0f67ade
Stuart Harris <stuartaharris@gmail.com>
2017-04-24 00:42:19 -0500

    add variables and turn array and reset

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-var turn = [];
+var turn=[];
+var counter = turn.length;
+var points = counter * 100;
+
+var roll = function () {
+  return Math.floor((Math.random() * 4) + 1);
+};
+
+var reset = function () {
+  turn = [];
+};
