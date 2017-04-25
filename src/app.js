c3b4d693eea3b350ecffa4d8bd7db95967053d48
Stuart Harris <stuartaharris@gmail.com>
2017-04-25 16:29:47 -0500

    add run func

--- src/app.js
+++ src/app.js
@@ -1,17 +1,10 @@
 //global variables and functions
 var turn = [];
-var counter;
-var points;
-var update = function () {
-  counter = turn.length;
-  points = (counter * counter) * 100;
-  };
 var roll = function () {
     return Math.floor((Math.random() * 4) + 1);
   };
 var next = function () {
     turn.push(roll());
-    update();
   };
 var reset = function () {
     turn = [];
@@ -20,14 +13,37 @@ var reset = function () {
 $(document).ready(function() {
 //begin jQuery logic
 var disp = function () {
-  update();
-  $(".round").text("Round: " + counter);
-  $(".score").text("Score: "+ points);
+  var a = turn.length;
+  $(".round").text("Round: " + a);
+  $(".score").text("Score: "+ (a * a) * 100);
+};
+var run = function () {
+  next();
+  disp();
+  for (var i = 0; i < turn.length; i++) {
+    var num;
+    if (turn[i] == 1) {
+      num = ".one";
+    }
+    else if (turn[i] == 2){
+      num = ".two";
+    }
+    else if (turn[i] == 3) {
+      num = ".three";
+    }
+    else {
+      num = ".four";
+    }
+    $(num).fadeOut(1500);
+    $(num).fadeIn(1500);
+
+  }
 };
 
 $('.start').on('click', function(event){
  event.preventDefault();
  next();
+ run();
  disp();
  });
 
