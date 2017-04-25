f5f586ef036c2dee312ec3392d95da218379da1b
Stuart Harris <stuartaharris@gmail.com>
2017-04-24 22:12:36 -0500

    add next, roll and reset

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 var turn=[];
-var counter = turn.length;
-var points = counter * 100;
-
+var update = function () {
+  counter = turn.length;
+  points = counter * 100;
+};
+var counter;
+var points;
+update();
 var roll = function () {
   return Math.floor((Math.random() * 4) + 1);
 };
-
+var next = function () {
+  turn.push(roll());
+  update();
+};
 var reset = function () {
   turn = [];
+  update();
 };
