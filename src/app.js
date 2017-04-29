c7a179a5dee06c64942c55f30d850742461c50cb
Stuart Harris <stuartaharris@gmail.com>
2017-04-29 13:56:31 -0500

    add sound

--- src/app.js
+++ src/app.js
@@ -12,42 +12,49 @@ var reset = function () {
 
 $(document).ready(function() {
 //begin jQuery logic
+var beep;
 var disp = function () {
   var a = turn.length;
   $(".round").text("Round: " + a);
   $(".score").text("Score: "+ (a * a) * 100);
 };
-var blink = function (num, colr) {
+var blink = function (num, colr, beep) {
     $(num).css("background-color", "white");
 //add audio
     setTimeout(function(){
         $(num).css("background-color", colr)
     }, 1000);
+        $(beep)[0].play();
 };
 var run = function () {
 
   for (var i = 0; i < turn.length; i++) {
     var num;
     var colr;
+
     if (turn[i] == 1) {
       num = ".one";
       colr = "red";
+      beep = "#one";
     }
     else if (turn[i] == 2){
       num = ".two";
       colr = "blue";
+      beep = "#two";
     }
     else if (turn[i] == 3) {
       num = ".three";
       colr = "green";
+      beep = "#three";
     }
     else {
       num = ".four";
       colr = "yellow";
+      beep = "#four";
     }
 
     setTimeout(function () {
-        blink(num, colr)}, 500);
+        blink(num, colr, beep)}, 500);
 
 
 
